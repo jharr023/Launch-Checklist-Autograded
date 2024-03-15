90a68fbd64e01f73b3cfb31c60fef041ae0ca986
jharr023 <jharringtonkc@gmail.com>
2024-03-14 20:21:43 -0500

    commit

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./s
 window.addEventListener("load", function() {
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch();
+    let listedPlanetsResponse = myFetch(); // Assuming myFetch() returns a promise that resolves with the list of planets
 
     // Once the data is fetched, continue execution
     listedPlanetsResponse.then(function(result) {
         listedPlanets = result;
         console.log(listedPlanets);
 
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        const randomPlanet = pickPlanet(listedPlanets); // Call pickPlanet() to select a random planet
+        // Select a random planet from the list of planets
+        const randomPlanet = pickPlanet(listedPlanets);
+
+        // Display the information of the selected planet
         addDestinationInfo(
             document,
             randomPlanet.name,
@@ -20,11 +22,9 @@ window.addEventListener("load", function() {
             randomPlanet.distance,
             randomPlanet.moons,
             randomPlanet.imageUrl
-        ); // Call addDestinationInfo() to display the selected planet's information
+        );
 
-        // Call formSubmission() with sample data to simulate form submission
-        formSubmission(document, list, "Pilot Name", "Co-pilot Name", 10000, 5000);
-    }).then(function () {
-        console.log(listedPlanets);
+        // Reload the page to see the mission target information
+        location.reload();
     });
 });
