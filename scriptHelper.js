e166f5fa9f0397d7eb493fc3fa886170c95dbc61
jharr023 <jharringtonkc@gmail.com>
2024-03-14 19:19:44 -0500

    commit

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,19 +3,19 @@
 require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
- }
+    const missionTargetDiv = document.getElementById('missionTarget');
+    missionTargetDiv.innerHTML = `
+        <h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${name}</li>
+            <li>Diameter: ${diameter}</li>
+            <li>Star: ${star}</li>
+            <li>Distance from Earth: ${distance}</li>
+            <li>Number of Moons: ${moons}</li>
+        </ol>
+        <img src="${imageUrl}">
+    `;
+}
  
  function validateInput(testInput) {
     if (testInput === "") {
@@ -82,19 +82,23 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         }
     }
 }
-module.exports.formSubmission = formSubmission;
  
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- }
- 
- function pickPlanet(planets) {
- }
+async function myFetch() {
+    const url = 'URL_TO_FETCH_PLANETS_DATA';
+    try {
+        const response = await fetch(url);
+        const planetsReturned = await response.json();
+        return planetsReturned;
+    } catch (error) {
+        console.error('Error fetching planetary data:', error);
+        return null;
+    }
+}
+
+function pickPlanet(planets) {
+    const randomIndex = Math.floor(Math.random() * planets.length);
+    return planets[randomIndex];
+}
  
  module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
